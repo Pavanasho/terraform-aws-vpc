da9b6b2c45413846f5a20dc6e67037a275a77d4d
Ashok <pavanashok5584@gmail.com>
2025-03-23 10:30:30 +0530

    vpc module added

--- variables.tf
+++ variables.tf
@@ -1,29 +1,31 @@
-variable "project_name" {
-  
+variable "project_name"{
+
 }
 
-variable "environment" {
-  
+variable "environment"{
+
 }
 
 variable "vpc_cidr" {
-  
+
 }
 
 variable "enable_dns_hostnames" {
-  
+    default = true
 }
 
-variable "common_tags" {
-  
+# mandatoryt
+variable "common_tags"{
+    type = map
+    #default = {}
 }
 
 variable "vpc_tags" {
-  
+    default = {}
 }
 
 variable "igw_tags" {
-  
+    default = {}
 }
 
 variable "public_subnet_cidrs" {
